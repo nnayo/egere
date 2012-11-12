f73f557e9aff3aa5dcfd9ad6c2b37da76280d663
yog <yog@b8f08ecd-264c-4901-8d74-809563748876>
2012-11-12 21:25:20 +0000

    transitions and states are in FLASH
    git-svn-id: svn://uranus/egere@6 b8f08ecd-264c-4901-8d74-809563748876

--- soft/vol/minut/main.c
+++ soft/vol/minut/main.c
@@ -1,6 +1,7 @@
 #include "minut.h"
 #include "servo.h"
-#include "acq.h"
+#include "mpu6050.h"
+#include "tk-off.h"
 
 #include "drivers/timer2.h"
 #include "utils/pt.h"
@@ -20,12 +21,128 @@
 #include "avr/io.h"
 #include "avr/interrupt.h"
 
+#if 0
+arduino	atmega	function
+-------+-------+--------
+
+D9	PB1	servo cone
+D10	PB2	servo aero
+
+D13	PB5	led alive
+D12	PB4	led open
+D11	PB3	cone open switch
+
+A4	PC4	sda
+A5	PC5	scl
+
++9V	PWR	power in
++5V	+5V	power out
+GND	GND	ground
+
+
+MPU-6050	function
+---------------+--------
+
+sda		sda
+scl		scl
+
++5V		power in
+GND		ground
+
+
+servo cône	function
+---------------+--------
+
++5V		power in
+ctrl		control signal
+GND		ground
+
+
+servo aero	function
+---------------+--------
+
++5V		power in
+ctrl		control signal
+GND		ground
+
+
+switch		function
+---------------+--------
+
+GND		ground
+sig		signal
+
+
+
+relay card	function
+---------------+--------
+
++9V		power in (accu)
+	GND		ground (accu)
+
+	+6V		power out (servo cone)
+	GND		ground (servo cone)
+	+6V		power out (servo aero)
+	GND		ground (servo aero)
+
+	+9V		power out (arduino)
+	+5V		power in (arduino)
+	GND		ground (arduino)
+
+	+5V		power out (MPU)
+	GND		ground (MPU)
+
+	sig		signal in (switch)
+	sig		signal out (arduino)
+	GND		ground (switch)
+
+	sda		sda (arduino)
+	sda		sda (MPU)
+	scl		scl(arduino)
+	scl		scl(MPU)
+
+	ctrl		ctrl servo aero (arduino)
+	ctrl		ctrl servo aero (servo)
+
+	ctrl		ctrl servo cone (arduino)
+	ctrl		ctrl servo cone (servo)
+
+	led alive	signal in (arduino)
+	led take-off	signal in (arduino)
+#endif
+
+// ------------------------------------------
+// simavr options
+//
+
+#include "avr_mcu_section.h"
+
+AVR_MCU(16000000, "atmega328p");
+
+const struct avr_mmcu_vcd_trace_t simavr_conf[]  _MMCU_ = {
+	{ AVR_MCU_VCD_SYMBOL("servo_cone"), .mask = _BV(PORTB1), .what = (void*)&PORTB, },
+	{ AVR_MCU_VCD_SYMBOL("servo_aero"), .mask = _BV(PORTB2), .what = (void*)&PORTB, },
+	{ AVR_MCU_VCD_SYMBOL("led_alive"), .mask = _BV(PORTB5), .what = (void*)&PORTB, },
+	{ AVR_MCU_VCD_SYMBOL("led_open"), .mask = _BV(PORTB4), .what = (void*)&PORTB, },
+	{ AVR_MCU_VCD_SYMBOL("cone_open_switch"), .mask = _BV(PORTB3), .what = (void*)&PINB, },
+	{ AVR_MCU_VCD_SYMBOL("TWDR"), .what = (void*)&TWDR, },
+	{ AVR_MCU_VCD_SYMBOL("TCNT1"), .what = (void*)&TCNT1, },
+	{ AVR_MCU_VCD_SYMBOL("OCR1AL"), .what = (void*)&OCR1AL, },
+	{ AVR_MCU_VCD_SYMBOL("OCR1AH"), .what = (void*)&OCR1AH, },
+	{ AVR_MCU_VCD_SYMBOL("OCR1BL"), .what = (void*)&OCR1BL, },
+	{ AVR_MCU_VCD_SYMBOL("OCR1BH"), .what = (void*)&OCR1BH, },
+	{ AVR_MCU_VCD_SYMBOL("TCCR1A"), .what = (void*)&TCCR1A, },
+	{ AVR_MCU_VCD_SYMBOL("TCCR1B"), .what = (void*)&TCCR1B, },
+	{ AVR_MCU_VCD_SYMBOL("TCNT2"), .what = (void*)&TCNT2, },
+};
+
 
 // ------------------------------------------
 // private definitions
 //
 
-#define TIMER2_TOP_VALUE	78
+//#define TIMER2_TOP_VALUE	78	// @ 8 MHz ==> 10 ms
+#define TIMER2_TOP_VALUE	156	// @ 16 MHz ==> 10 ms
 
 
 // ------------------------------------------
@@ -103,7 +220,8 @@ int main(void)
 
 	MNT_init();
 	SERVO_init();
-	ACQ_init();
+	MPU_init();
+	TKF_init();
 
 	// enable interrupts
 	sei();
@@ -123,7 +241,14 @@ int main(void)
 
 		MNT_run();
 		SERVO_run();
-		ACQ_run();
+		MPU_run();
+		TKF_run();
+
+		if ( TIME_get() > 10 * TIME_1_SEC ) {
+			cli();
+#include <avr/sleep.h>
+			sleep_mode();
+		}
 	}
 
 	// this point is never reached
