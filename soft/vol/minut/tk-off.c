5903505006eb0c02291014eadfe36bb222683d41
yog <yog@b8f08ecd-264c-4901-8d74-809563748876>
2013-03-08 21:46:26 +0000

    madgwick algo to be used for take-off detection
    git-svn-id: svn://uranus/egere@21 b8f08ecd-264c-4901-8d74-809563748876

--- soft/vol/minut/tk-off.c
+++ soft/vol/minut/tk-off.c
@@ -15,6 +15,8 @@
 
 #define IN_FIFO_SIZE	1
 
+#define SAMPLEFREQ		100.0f	// Hz
+
 
 // ------------------------------------------
 // private variables
@@ -32,12 +34,138 @@ struct {
 	u8 thr_duration;			// threshold duration in 0.1s
 	u16 acc_thr;				// acceleration threshold in 0.1G
 	u8 thr_flag;
+
+	float acc_x;
+	float acc_y;
+	float acc_z;
+	float gyr_x;
+	float gyr_y;
+	float gyr_z;
+
+	// quaternion computation using Madgwick's IMU and AHRS algorithms.
+	float beta;					// 2 * proportional gain (Kp)
+	float q0, q1, q2, q3;		// quaternion of sensor frame relative to auxiliary frame
 } TKF;
 
 
 // ------------------------------------------
 // private functions
 //
+
+// Fast inverse square-root
+// See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+
+float inv_sqrt(float x)
+{
+	union {
+		float f;
+		long l;
+	} conv_f2l;
+
+	float halfx = 0.5f * x;
+	float y = x;
+
+	//long i = *(long*)&y;
+	conv_f2l.f = y;
+	long i = conv_f2l.l;
+
+	i = 0x5f3759df - (i>>1);
+	//y = *(float*)&i;
+	conv_f2l.l = i;
+	y = conv_f2l.f;
+
+	y = y * (1.5f - (halfx * y * y));
+
+	return y;
+}
+
+
+static void TKF_Madgwick(void)
+{
+	float recipNorm;
+	float s0, s1, s2, s3;
+	float qDot1, qDot2, qDot3, qDot4;
+	float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+
+	float q0 = TKF.q0;
+	float q1 = TKF.q1;
+	float q2 = TKF.q2;
+	float q3 = TKF.q3;
+
+	float ax = TKF.acc_x;
+	float ay = TKF.acc_y;
+	float az = TKF.acc_z;
+	float gx = TKF.gyr_x;
+	float gy = TKF.gyr_y;
+	float gz = TKF.gyr_z;
+
+	// Rate of change of quaternion from gyroscope
+	qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
+	qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
+	qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
+	qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+
+	// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+	if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+
+		// Normalise accelerometer measurement
+		recipNorm = inv_sqrt(ax * ax + ay * ay + az * az);
+		ax *= recipNorm;
+		ay *= recipNorm;
+		az *= recipNorm;   
+
+		// Auxiliary variables to avoid repeated arithmetic
+		_2q0 = 2.0f * q0;
+		_2q1 = 2.0f * q1;
+		_2q2 = 2.0f * q2;
+		_2q3 = 2.0f * q3;
+		_4q0 = 4.0f * q0;
+		_4q1 = 4.0f * q1;
+		_4q2 = 4.0f * q2;
+		_8q1 = 8.0f * q1;
+		_8q2 = 8.0f * q2;
+		q0q0 = q0 * q0;
+		q1q1 = q1 * q1;
+		q2q2 = q2 * q2;
+		q3q3 = q3 * q3;
+
+		// Gradient decent algorithm corrective step
+		s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+		s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+		s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+		s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+		recipNorm = inv_sqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+		s0 *= recipNorm;
+		s1 *= recipNorm;
+		s2 *= recipNorm;
+		s3 *= recipNorm;
+
+		// Apply feedback step
+		qDot1 -= TKF.beta * s0;
+		qDot2 -= TKF.beta * s1;
+		qDot3 -= TKF.beta * s2;
+		qDot4 -= TKF.beta * s3;
+	}
+
+	// Integrate rate of change of quaternion to yield quaternion
+	q0 += qDot1 * (1.0f / SAMPLEFREQ);
+	q1 += qDot2 * (1.0f / SAMPLEFREQ);
+	q2 += qDot3 * (1.0f / SAMPLEFREQ);
+	q3 += qDot4 * (1.0f / SAMPLEFREQ);
+
+	// Normalise quaternion
+	recipNorm = inv_sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+	q0 *= recipNorm;
+	q1 *= recipNorm;
+	q2 *= recipNorm;
+	q3 *= recipNorm;
+
+	TKF.q0 = q0;
+	TKF.q1 = q1;
+	TKF.q2 = q2;
+	TKF.q3 = q3;
+}
+
 
 // update threshold configuration from incoming command
 static void TKF_config(void)
@@ -85,6 +213,8 @@ static u8 TKF_compute(void)
 static PT_THREAD( TKF_thread(pt_t* pt) )
 {
 	frame_t fr;
+	s16 acc;
+	s16 gyr;
 
 	PT_BEGIN(pt);
 
@@ -105,6 +235,18 @@ static PT_THREAD( TKF_thread(pt_t* pt) )
 
 	// acceleration data
 	case FR_DATA_ACC:
+		acc = TKF.in_fr.argv[0] << 8;
+		acc += TKF.in_fr.argv[1] << 0;
+		TKF.acc_x = (float)acc;
+
+		acc = TKF.in_fr.argv[2] << 8;
+		acc += TKF.in_fr.argv[3] << 0;
+		TKF.acc_y = (float)acc;
+
+		acc = TKF.in_fr.argv[4] << 8;
+		acc += TKF.in_fr.argv[5] << 0;
+		TKF.acc_z = (float)acc;
+
 		if ( KO == TKF_compute()) {
 			PT_RESTART(pt);
 		}
@@ -112,9 +254,21 @@ static PT_THREAD( TKF_thread(pt_t* pt) )
 
 	// rotation data
 	case FR_DATA_GYR:
-		if ( KO == TKF_compute()) {
-			PT_RESTART(pt);
-		}
+		gyr = TKF.in_fr.argv[0] << 8;
+		gyr += TKF.in_fr.argv[1] << 0;
+		TKF.gyr_x = (float)gyr;
+
+		gyr = TKF.in_fr.argv[2] << 8;
+		gyr += TKF.in_fr.argv[3] << 0;
+		TKF.gyr_y = (float)gyr;
+
+		gyr = TKF.in_fr.argv[4] << 8;
+		gyr += TKF.in_fr.argv[5] << 0;
+		TKF.gyr_z = (float)gyr;
+
+		TKF_Madgwick();
+
+		PT_RESTART(pt);
 		break;
 
 	default:
@@ -152,6 +306,13 @@ void TKF_init(void)
 	TKF.thr_time_out = TIME_MAX;
 	TKF.thr_duration = 255;	// 25.5s
 	TKF.acc_thr = 20;	// 2.0G
+
+	// quaternion init
+	TKF.beta = 0.1f;
+	TKF.q0 = 1.0;
+	TKF.q1 = 0.0;
+	TKF.q2 = 0.0;
+	TKF.q3 = 0.0;
 
 	PT_INIT(&TKF.pt);
 }
