b99ac7922d09eb9d0b62c813cd3a91b4557969f9
yog <yog@b8f08ecd-264c-4901-8d74-809563748876>
2013-03-29 20:54:49 +0000

    correct mnemo names and values
    git-svn-id: svn://uranus/egere@36 b8f08ecd-264c-4901-8d74-809563748876

--- soft/minut.py
+++ soft/minut.py
@@ -7,11 +7,11 @@ from frame import *
 from frame import Frame
 
 
-CONE_SERVO = 0xaa
-CONE_AERO = 0x55
+SERVO_CONE = 0xc0
+SERVO_AERO = 0xae
 
-SERVO_SAVE = 0x00
-SERVO_READ = 0xff
+SERVO_SAVE = 0x5a
+SERVO_READ = 0x4e
 
 SERVO_OPEN_POS = 0x09
 SERVO_CLOSE_POS = 0xc1
@@ -31,19 +31,19 @@ slots = [
 	# slot #0 : reset
 	[
 		# set cone servo open position: -15deg
-		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, CONE_SERVO, SERVO_SAVE, SERVO_OPEN_POS, -15),
+		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, SERVO_CONE, SERVO_SAVE, SERVO_OPEN_POS, -15),
 
 		# set cone servo closed position: +30deg
-		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, CONE_SERVO, SERVO_SAVE, SERVO_CLOSE_POS, 30),
+		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, SERVO_CONE, SERVO_SAVE, SERVO_CLOSE_POS, 30),
 
 		# set aero servo open position: -15deg
-		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, CONE_AERO, SERVO_SAVE, SERVO_OPEN_POS, -15),
+		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, SERVO_AERO, SERVO_SAVE, SERVO_OPEN_POS, -15),
 
 		# set aero servo closed position: +30deg
-		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, CONE_AERO, SERVO_SAVE, SERVO_CLOSE_POS, 30),
+		minut_servo_info(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, SERVO_AERO, SERVO_SAVE, SERVO_CLOSE_POS, 30),
 
-		# set flight time-out: 12.5s
-		minut_time_out(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, TIME_OUT_SAVE, 125),
+		# set flight time-out: 4.5s
+		minut_time_out(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, TIME_OUT_SAVE, 45),
 
 		# set flight take-off detection threshold (10 * 10ms, 30 * 0.1G)
 		take_off_thres(I2C_SELF_ADDR, I2C_SELF_ADDR, T_ID, CMD, 10, 30),
